f2d94316d1126d55419fc99da9587f61c3f39ca8
ryan28561 <ryan28561@gmail.com>
2019-11-09 12:40:51 -0700

    added utility for hashing and storing passwords

---
+++ hashPass.py
@@ -0,0 +1,41 @@
+import hashlib
+import uuid
+
+def generate_salt():
+    return str(uuid.uuid4())
+
+def hash_password(password):
+    salt = generate_salt()
+    md5 = hashlib.md5()
+    md5.update((salt + password).encode('ascii'))
+    return salt, md5.hexdigest()
+
+def store_password(plain_pass):
+    salt, hsh = hash_password(plain_pass)
+    with open('password_hashes.txt', 'a') as f:
+        f.write(hsh + "\n")
+    with open('password_salts.txt', 'a') as f:
+        f.write(salt + "\n")
+
+def check_password(plain_pass):
+    hashes = []
+    salts = []
+    try:
+        with open('password_hashes.txt', 'r+') as f:
+            hashes = f.readlines()
+        with open('password_salts.txt', 'r+') as f:
+            salts = f.readlines()
+    except Exception:
+        print('no passwords')
+
+    for i in range(len(hashes)):
+        md5 = hashlib.md5()
+        md5.update((salts[i] + plain_pass).encode('ascii'))
+        if md5.hexdigest() == hashes[i]:
+            print('User {} Authenticated'.format(i))
+            return True
+
+    print("No matching password found.")
+    return False
+
+
