ce682a053fdcf109e06f71d857e19fdc92694069
henok <henarks@gmail.com>
2019-11-10 15:08:01 -0700

    task 2

---
+++ PersonalPasswordCreator.py
@@ -0,0 +1,125 @@
+personlInfo = {'First Name': 'Jon', 'Last Name': 'Doe', 'DOB': '10/09/1992', 'Phone':'208-123-456', 'Address': {'Street Name': '2660 Main st', 'Apartment Number': '20', 'City': 'Logan', 'State': 'UT', 'Zip': '84321'}, 'Email ID': 'JonDoe@gmail.com'}
+
+#FirstName
+def fName_password(s):
+    return s
+
+#LastName
+def lName_password(s):
+    return 
+
+#First Part of Date of birth includinga all
+def firstPartDateOfBirth_password(s):
+    firstPart = s[:random.randint(2, len(string)+1)]
+    
+    return firstPart
+
+#Second part of date of birth
+def secondPartDateOfirth_password(s):
+    secondPart = s[random.randint(2,len(string)-1):]
+    return secondPart
+
+#First part of phone including all digits
+def firstPartPhone_password(s):
+    digits = s.strip('-')
+    fristPart = digits[:random.randint(2,len(digits)+1)]
+    return firstPart
+
+#Second part of phone
+def secondPartPhone_password(s):
+    digits = s.strip('-')
+    secondPart = digits[random.randint(2, len(digits)-1):]
+    return secondPart
+
+#Street number
+def streetNumber_password(s):
+    string = s.split(" ", 1)[0]
+    return string
+
+#Street Name
+def streetName_password(s):
+    string = s.split(" ", 1)[1]
+    return string
+
+#Apartment Number
+def aprtment_password(s):
+    if s:
+        return s
+    else:
+        return
+
+#city
+def city_password(s):
+    return s
+
+#state
+def state_password(s):
+    return s
+
+#zip code
+def zip_password(s):
+    return s
+
+#Email id
+def emailId_password(s):
+    email = s.spilt("@")[0]
+    string = email[:random.randint(2, len(email))+1]
+    return string
+
+#list creator
+def enhanced_password_list_creator2(personalInfo):
+    passwords = open("enhancedPasswordList2.txt", "a")
+    final = ""
+
+    #Name
+    firstName = fname_password(personalInfo['First Name'])
+    final += firstName + '\n'
+
+    lastName = lname_password(personalInfo['Last Name'])
+    final += lastName + '\n'
+    #Date of birth
+    firstPartofDateOfBirth = firstPartDateOfBirth_password(personalInfo['DOB'])
+    final += firstPartOfDateOfBirth + '\n'
+
+    secondPartDateOfBirth = secondPartDateOfBirth_password(personalInfo['DOB'])
+    final += secondPartDateOfBirth + '\n'
+    #Phone
+    firstPartPhone = firstPartPhone_password(personalInfo['Phone'])
+    final += firstPartPhone + '\n'
+
+    secondPartPhone = secondPartPhone_password(personalInfo['Phone'])
+    final += secondPartPhone + '\n'
+    #Address
+    address = personalInfo['Address']
+    streetNumber = streeNumber_password(address['Street Name'])
+    final += streetNumber + '\n'
+
+    streetName = streetName_password(address['Street Name'])
+    final += street Name + '\n'
+
+    apartment = apartment_password(address['Apartment Number'])
+    final += apartment + '\n'
+    
+    city = city_password(address['City'])
+    final += city + '\n'
+
+    state = state_password(address['State'])
+    final += State + '\n'
+    
+    zipCode = zip_password(address['Zip'])
+    final += zipCode + '\n'
+    #email Id
+    emailId = emailId_password(personalInfo['Email ID'])
+    final += emailId + '\n'
+
+    passwords.write(final)
+    passwords.close
+
+
+
+
+
+    
+
+
+        
